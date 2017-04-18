cc2d3258a82b05a6d4afb4681ae8beba826a60e3
Artem Savienkov <savienkov.ao@gmail.com>
2017-04-18 11:53:22 +0300

    Some corrections added

---
+++ data/data.js
@@ -0,0 +1,15 @@
+var Data=[
+		{id:1,name:'Name 1', city:1, category:2, price:50},
+		{id:2,name:'Name 2', city:4, category:1, price:100},
+		{id:3,name:'Name 3', city:5, category:1, price:1},
+		{id:4,name:'Name 4', city:2, category:4, price:150},
+		{id:5,name:'Name 5', city:3, category:5, price:200},
+		{id:6,name:'Name 6', city:1, category:2, price:50},
+		{id:7,name:'Name 7', city:4, category:1, price:100},
+		{id:8,name:'Name 8', city:5, category:1, price:1},
+		{id:9,name:'Name 9', city:2, category:4, price:150},
+		{id:10,name:'Name 10', city:1, category:5, price:1},
+		{id:11,name:'Name 11', city:2, category:3, price:23},
+		{id:12,name:'Name 12', city:1, category:5, price:56},
+		{id:13,name:'Name 13', city:5, category:1, price:12}
+	];
---
+++ src/js/main.js
@@ -0,0 +1,217 @@
+// global vars
+
+var decrypData=[];
+var decrypCategory=[];
+
+$(function () {
+	// Filter toggle
+	
+	$("#filter-toggle").on('click', function() {
+	
+		$("#filter").toggle();
+	});
+
+	$(window).on('resize', resizeFunc);
+
+	function resizeFunc() {
+		if( $(window).width() > 786) $("#filter").removeAttr('style');
+	}
+
+
+	// displaying City elements
+
+	var cityTempl = $('#cityTempl').html();
+	var cityContent = tmpl(cityTempl,City) ;
+
+	$("#filter-city").append(cityContent);
+
+	// displaying Category elements
+	mapCategory();
+	var categoryTempl = $('#categoryTempl').html();
+	var categoryContent = tmpl(categoryTempl,decrypCategory) ;
+
+	$("#filter-category").append(categoryContent);
+
+
+	// displaying Data elements
+	mapData();
+	var dataTempl = $('#dataTempl').html();
+	var dataContent = tmpl(dataTempl,decrypData) ;
+
+	$("#filter-data").append(dataContent);
+
+
+
+// decrypting Data and filling decrypData
+	function mapData() {
+	
+		Data.forEach(function (item, i, arr){
+			var newItem = $.extend(true, {}, item);
+			decrypData.push(newItem);
+		});
+
+		decrypData.map(function (item, i, arr){
+			item.city = getCityName(item.city);
+			item.category = getCategoryName(item.category);
+		});
+
+	}	
+
+
+	function getCityName(argId) {
+		for (var i =0; i < City.length; i++) {
+			if(City[i].id == argId){				
+				return City[i].name;
+			}
+		}
+	}
+
+	function getCategoryName(argId) {
+		for (var i = 0; i < Category.length; i++) {
+			if(Category[i].id == argId){				
+				return Category[i].name;
+			}
+		}
+	}
+
+
+
+// decrypting Category and filling decrypCategory
+	function mapCategory() {
+		Category.forEach(function (item, i, arr){
+			decrypCategory.push(item);
+			item.count = 0;
+			item.count = getCategoryCount(item.id, item.count);
+		});
+	}
+
+	function getCategoryCount(id, count) {
+		for (var i =0; i < Data.length; i++) {
+			if(Data[i].category == id){		
+				count++;
+			}
+		}
+		return count;
+	}
+
+
+
+
+
+
+// form processing
+
+
+$( "form" ).on( "submit", function( event ) {
+
+  event.preventDefault();
+  var formResult = $(this).serializeArray();
+	var filterCity=[];
+	var filterCategory=[];
+	console.log(formResult);
+
+	formResult.forEach(function (item, i, arr){
+		if(formResult.length ==1){
+			getFilterCity( (parseInt(item.value)) );
+			filterCategory = filterCity;
+		} else {
+			if(item.name == 'city'){
+				getFilterCity( (parseInt(item.value)) );
+			}else if(item.name == 'category'){
+				getFilterCategory( (parseInt(item.value)) );
+			}			
+		}
+		
+	});
+
+// filter by city name
+	function getFilterCity(filterValue){
+
+		Data.forEach(function(item, i, arr) {
+			if (item.city === filterValue) {
+				filterCity.push(item);	
+			}
+		});
+	}
+
+// filter by category name
+	function getFilterCategory(filterValue){
+
+		filterCity.forEach(function(item, i, arr) {
+			if (item.category === filterValue) {
+				filterCategory.push(item);	
+			}
+		});
+	}
+
+console.log(filterCategory);
+
+	filterCategory.map(function (item, i, arr){
+		item.city = getCityName(item.city);
+		item.category = getCategoryName(item.category);
+	});
+
+// displaying filtered elements
+
+	$("#filter-data").children().remove();
+	addElements();
+	
+	function addElements(){
+
+		if( filterCategory.length > 0){
+			decrypData = filterCategory;
+			dataContent = tmpl(dataTempl, decrypData);
+			console.log(filterCategory);
+		} else if (filterCategory.length == 0){
+			dataContent = '<p>There is no matched elements</p>';
+			console.log(filterCategory);
+		}
+
+		$("#filter-data").append(dataContent);
+	}
+
+});
+
+
+
+// get min&max values for slider range
+	
+	function getPrices() {
+		var pricesArray = [];
+
+		Data.forEach(	function (item, i, arr) {
+			pricesArray.push(item.price);
+		});
+
+		return pricesArray;
+	}
+
+	function getMinPrice() {
+		var minArray = getPrices();
+		return Math.min.apply( Math, minArray );
+	}
+
+	function getMaxPrice() {
+		var maxArray = getPrices();
+		return Math.max.apply( Math, maxArray );
+	}
+
+
+// slider range    
+
+	$( "#slider-range" ).slider({
+    range: true,
+    min: getMinPrice(),
+    max: getMaxPrice(),
+    values: [0, 250 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    }
+  });
+  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+
+
+});
+
+
